682bc35c428e878a75b2c1b4e89a64f60b404c47
islammahmou <131728429+islammahmou@users.noreply.github.com>
2024-01-07 17:11:04 -0500

    Update code_to_edit.py

--- code_to_edit.py
+++ code_to_edit.py
@@ -5,6 +5,6 @@ def length_of_string(x):
     ## TODO: Write a code that takes as an input string (x) returns the length of the string
     return 0 # Replace 0 by the length
 
-def to_numbers_equal(x,y):
+def two_numbers_equal(x,y):
     ## TODO: Write a code that takes as an inputs two numbers (x,y) and returns True if x is equal to y and False otherwise
     return True # Replace True by the needed return
