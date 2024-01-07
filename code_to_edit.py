4aa45312467668fe6dad300b7586826e83c6195b
islammahmou <131728429+islammahmou@users.noreply.github.com>
2024-01-07 17:33:01 -0500

    Update code_to_edit.py

--- code_to_edit.py
+++ code_to_edit.py
@@ -1,11 +1,11 @@
 def hi_my_name_is():
-    return "" # Write your name inside the quotation marks
+    return "Islam" # Write your name inside the quotation marks
 
 
 def length_of_string(x):
     ## TODO: Write a code that takes as an input string (x) returns the length of the string
-    return 0 # Replace 0 by the length
+    return len(x) # Replace 0 by the length
 
 def two_numbers_equal(x,y):
     ## TODO: Write a code that takes as an inputs two numbers (x,y) and returns True if x is equal to y and False otherwise
-    return True # Replace True by the needed return
+    return x==y # Replace True by the needed return
